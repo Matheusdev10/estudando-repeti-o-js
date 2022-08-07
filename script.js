3e5cb93716e0db023eb263244fb29693926c22b3
Matheus <matheus_501@hotmail.com>
2022-08-07 10:20:08 -0300

    estudando estrutura de repeticao js

---
+++ script.js
@@ -0,0 +1,52 @@
+// --------------------FOR------------------
+
+// break para a execução do loop
+// continue pula a execução do momento e continua
+
+// for (let i = 10; i >= 0; i--) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//   if (i === 5) {
+//     contimue;
+//   }
+//   console.log(i);
+// }
+
+// --------------------WHILE------------
+// while é usado quando nao se sabe o momento de parada
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// ----------------FOR OF------------
+// let name = 'Matheus';
+// let names = ['Joao', 'Paulo', 'Pedro'];
+
+// for (let char of name) {
+//   console.log(char);
+// }
+
+// for (let name of names) {
+//   console.log(name);
+// }
+
+// ----------------FOR IN------------
+// cria um loop em cima de um objeto
+
+// let person = {
+//   name: 'John',
+//   age: 30,
+//   wwight: 88.6,
+// };
+
+// for (let property in person) {
+//   console.log(property);
+//   console.log(person[property]);
+// }
